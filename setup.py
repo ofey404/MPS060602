06a36bde1ea1a4b1aa6edd36285e855d6782d840
Ofey Chan <ofey206@gmail.com>
2022-03-16 14:41:36 +0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             raise
         setup(
             use_scm_version={"version_scheme": "no-guess-dev"},
-            version="0.1",
+            version="0.0.1",
             author="Weiwen Chen",
             author_email="ofey206@gmail.com",
             license="MIT",
